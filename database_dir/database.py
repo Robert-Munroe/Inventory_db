4e6b23021fa9ca741e8fa23aef7995d568532ab5
RMunroe943406 <rmunroe@sutdent.bridgew.edu>
2022-08-04 11:34:03 -0400

    changed location of database, added storage_locations.py

--- database_dir/database.py
+++ database_dir/database.py
@@ -4,7 +4,7 @@ from gui_dir import gui_windows
 
 
 def db_location():
-    location = r"D:\foundersinventorydb.sqlite"
+    location = r"\\FSGFS\Shared Folders\Access\foundersinventorydb\foundersinventorydb.sqlite"
     return location
 
 
