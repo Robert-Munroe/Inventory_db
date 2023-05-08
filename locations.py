45dbc0869dba1f01d0700bc29df632dfea829e6f
RMunroe943406 <54950744+Robert-Munroe@users.noreply.github.com>
2023-05-08 10:36:19 -0400

    Delete locations.py

--- locations.py
+++
@@ -1,6 +0,0 @@
-def set_founders_location():
-    return r"\\FSGFS\Shared Folders\Access\foundersinventorydb\foundersinventorydb.sqlite"
-
-
-def set_personal_location():
-    return r"I:\database\foundersinventorydb.sqlite"
